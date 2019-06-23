a614fb117e2109a14daeaf08ec5305ce4fa8ff41
Michael Slowik <slowik.michael@gmail.com>
2019-06-23 20:24:00 +0200

    code clean-up

--- src/FirestoreSessionServiceProvider.php
+++ src/FirestoreSessionServiceProvider.php
@@ -10,16 +10,6 @@ use Illuminate\Support\ServiceProvider;
 class FirestoreSessionServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
      * Bootstrap services.
      *
      * @return void
